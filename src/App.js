bc06086c971492c3570f35fb310edc108305ea78
Mateusz <mateuszb430@gmail.com>
2023-11-24 10:59:17 +0100

    Added Header with css styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import {Header} from "./components/Header/Header";
+
+
+
+
 
 export function App() {
   return (
     <div className="App">
 
-      <img src="logo.webp" alt="megaK"/>
+      {/*<img src="logo.webp" alt="megaK"/>*/}
+
+       <Header/>
 
     </div>
   );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+
+
+import "./Header.css"
+
+
+export const Header = () => {
+
+
+
+    return <div id="header">
+        <div id="header-inside">
+
+        <img id="logo" src="logo.webp" alt="megaK"/>
+
+        <div id="user">Tester Testowy</div>
+
+    </div>
+    </div>
+}
